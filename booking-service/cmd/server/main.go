4938f748dd78ff5ec185f5df0b179e24d7d33a12
artitaya <artitaya2466@gmail.com>
2026-04-17 13:29:21 +0700

    update config

--- booking-service/cmd/server/main.go
+++ booking-service/cmd/server/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
 
 	"github.com/Aritiaya50217/High-Concurrency-Ticket-Booking-System/booking-service/internal/infrastructure/config"
 	"github.com/Aritiaya50217/High-Concurrency-Ticket-Booking-System/booking-service/internal/infrastructure/database"
@@ -10,17 +11,34 @@ import (
 	handlerBooking "github.com/Aritiaya50217/High-Concurrency-Ticket-Booking-System/booking-service/internal/interface/handler"
 	"github.com/Aritiaya50217/High-Concurrency-Ticket-Booking-System/booking-service/internal/interface/router"
 	usecaseBooking "github.com/Aritiaya50217/High-Concurrency-Ticket-Booking-System/booking-service/internal/usecase"
+	"github.com/joho/godotenv"
 )
 
 func main() {
-	cfg, err := config.LoadConfig("configs/config.yaml")
+	// โหลด .env
+	envPath := os.Getenv("ENV_PATH")
+	if envPath == "" {
+		envPath = ".env"
+	}
+
+	configPath := os.Getenv("CONFIG_PATH")
+	if configPath == "" {
+		configPath = "configs/config.yaml"
+	}
+
+	godotenv.Load(envPath)
+
+	cfg, err := config.LoadConfig(configPath)
 	if err != nil {
 		log.Fatal(err)
 	}
 	log.Println("App : ", cfg.App.Port)
 
 	// connect to database
-	db := database.NewPostgresDB(cfg)
+	db, err := database.NewPostgresDB(cfg)
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	repoBooking := repositoryBooking.NewBookingRepository(db)
 	usecaseBooking := usecaseBooking.NewBookingUsecase(repoBooking)
@@ -29,4 +47,7 @@ func main() {
 	router := router.SetupRouter(handlerBooking)
 
 	router.Run(fmt.Sprintf(":%d", cfg.App.Port))
+	if err := router.Run(fmt.Sprintf(":%d", cfg.App.Port)); err != nil {
+		log.Fatal(err)
+	}
 }
